9695361017110a2bbdd633a193c2ec7c1b371bcd
SamiKr <44946069+SamiKr@users.noreply.github.com>
2022-10-27 23:13:18 +0200

    Add files via upload

---
+++ Scripting/test.ps1
@@ -0,0 +1,14 @@
+﻿$MXS = Resolve-DnsName -Type MX -Name "esdown.fr" -ErrorAction SilentlyContinue | ? Type -eq MX | Select @{L="Host"; E={$_.NameExchange}}, Preference | Sort-Object Preference 
+Remove-Item "C:\Users\ASUS\Desktop\Nouveau dossier\ItWorks.csv"
+
+foreach($MX in $MXS.Host) {
+    Write-Host $MX
+
+    $MXDiag = [PSCustomObject] @{
+        Domain = "esdown.fr"
+        MX = $MX
+        SendMail = "False"
+    }
+
+    Write-Output $MXDiag | Export-Csv -Path "C:\Users\ASUS\Desktop\Nouveau dossier\ItWorks.csv" -NoTypeInformation -Append
+}
